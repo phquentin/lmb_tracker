0600d23a9ecc85816849c6a3967996d1a1813de7
maxschnettler <max.schnettler@gmx.de>
2020-11-06 11:45:42 +0100

    Add initial target class dummy

---
+++ lmb/target.py
@@ -0,0 +1,24 @@
+class Target():
+    """
+    Represents a single target
+
+    A target is represented by its label, an existence probability, and 
+    a probability density function describing the current target state.
+    """
+    def __init__.py(self, label, r=0, pdf=None):
+        self.label = label
+        self.r = r
+        self.pdf = pdf
+
+    def predict(self):
+        """
+        Predict the state to the next time step
+        """
+        pass
+
+    def correct(self):
+        """
+        Correct the target state based on the new measurement
+        """
+        pass
+
