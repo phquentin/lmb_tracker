8d528cfb8e04e26aecdb410f0d2bea5c20035fe4
maxschnettler <max.schnettler@gmx.de>
2020-11-10 17:33:38 +0100

    Add default parameters for state estimation using Kalman filter

--- lmb/parameters.py
+++ lmb/parameters.py
@@ -6,6 +6,8 @@ class Parameters():
     """
     Class containing the overal tracker parameters
     """
+    dim_x: int = 4              # Dimension (number) of states (first + second order)
+    dim_z: int = 2              # Dimension (number) of measurement inputs
     n_targets_max: int = 1000   # maximum number of targets
     n_gm_cmpnts_max: int = 100  # maximum number of Gaussian mixture components
     p_survival: float = 0.99    # survival probability
@@ -17,7 +19,20 @@ class Parameters():
     R: np.ndarray = np.asarray([[10., 0.],
                                 [0., 10.]], dtype='f4')
     # process noise covariance
-    Q: np.ndarray = np.asarray([[5, 0, 10, 0],
-                                [0, 5, 0, 10],
-                                [10, 0, 20, 0],
-                                [0, 10, 0, 20]], dtype='f4')
+    Q: np.ndarray = np.asarray([[5., 0., 10., 0.],
+                                [0., 5., 0., 10.],
+                                [10., 0., 20., 0.],
+                                [0., 10., 0., 20.]], dtype='f4')
+    # Motion model: state transition matrix
+    F: np.ndarray = np.asarray([[1., 0., 1., 0.],
+                                [0., 1., 0., 1.],
+                                [0., 0., 1., 0.],
+                                [0., 0., 0., 1.]], dtype='f4')
+    # Observation model
+    H: np.ndarray = np.asarray([[1., 0., 0., 0.],
+                                [0., 1., 0., 0.]], dtype='f4')
+    # Initial state covariance matrix
+    P_init: np.ndarray = np.asarray([[100., 0., 0., 0.],
+                                    [0., 100., 0., 0.],
+                                    [0., 0., 100., 0.],
+                                    [0., 0., 0., 100.]], dtype='f4')
